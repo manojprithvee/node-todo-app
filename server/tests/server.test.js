01298c923b1e4241763bc8c245457b8f1e949b2f
Divakar Manoj <manoj.prithvee@gmail.com>
2017-11-26 23:53:15 +0530

    Test GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,12 @@ const request = require("supertest");
 const {app}=require("./../server")
 const {Todo}=require("./../models/todo")
 
+var todoarray=[{text:"manoj is a good boy"},{text:"ashwin is a good boy"}]
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+        Todo.insertMany(todoarray);
+    }).then(()=> done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +28,7 @@ describe('POST /todos', () => {
             return done(err);
           }
   
-          Todo.find().then((todos) => {
+          Todo.find({text}).then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
             done();
@@ -42,10 +46,24 @@ describe('POST /todos', () => {
           }
   
           Todo.find().then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           }).catch((e) => done(e));
         });
 
     })
+});
+
+describe('GET /todos', () => {
+    it('should create a new todo', (done) => {
+      var text = 'Test todo text';
+  
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+        })
+        .end((err, res) => done());
+    });
 });
