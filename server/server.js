2dcdbaecfb0e96a9201edf6e07e0a485bc62f318
Divakar Manoj <manoj.prithvee@gmail.com>
2017-11-27 06:09:07 +0530

    PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
-var express = require("express");
-var BodyParser = require("body-parser");
+const env = process.env.NODE_ENV || "development";
+
+if(env==="development"){
+    process.env.PORT=3000
+    process.env.MONGODB_URI="mongodb://localhost:27017/TodoApp"
+}else if(env==="test"){
+    process.env.PORT=3000
+    process.env.MONGODB_URI="mongodb://localhost:27017/TodoAppTest"
+}
+
+const _ = require("lodash")
+const express = require("express");
+const BodyParser = require("body-parser");
 const {ObjectID} = require("mongodb")
 
 require("./db/mongoose");
@@ -50,14 +61,39 @@ app.delete("/todos/:id",(req,res)=>{
         if (!todo){
             return res.status(404).send()
         }
-        res.send(todo)
+        res.send({todo})
     }).catch((error)=>{
         res.send()
     })
 
 });
+
+app.patch("/todos/:id",(req,res)=>{
+    var id=req.params.id;
+    var body=_.pick(req.body, ["text","completed"]);
+    if (!ObjectID.isValid(id))
+    {
+        return res.status(404).send()
+    }
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt=new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt=null;
+    }
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+        if (!todo){
+            return res.status(404).send()
+        }
+        res.send({todo})
+    }).catch((error)=>{
+        res.status(400).send()
+    })
+});
 app.listen(process.env.PORT||3000,()=>{
-    console.log(`Server running on port ${process.env.PORT||3000}`,)
+    console.log(`Server running on ${env} port ${process.env.PORT||3000}`,)
 });
 
 module.exports={
